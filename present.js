d40261697b293d5980e14a10156aba8d389e71e3
Nguyenvantuan <133848204+Tuanbautroi@users.noreply.github.com>
2023-09-26 17:10:56 +0700

    present.js

--- present.js
+++ present.js
@@ -1,7 +1,7 @@
 
 var to = '🐷!';
 var gift_url = 'https://www.facebook.com/nguyenbaolan1.10.20';
-var gift_image_url = 'https://scontent.fhan2-3.fna.fbcdn.net/v/t39.30808-6/380996014_1124269575626822_4277449062801180984_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5614bc&_nc_ohc=QM9xNruvh9sAX9wE1OS&_nc_ht=scontent.fhan2-3.fna&oh=00_AfAitjNYGDKEwy_16hSZiNrvZMe0S4k8vJNuAMhRo2GYjg&oe=650F1624';
+var gift_image_url = 'https://scontent.fhan5-3.fna.fbcdn.net/v/t39.30808-6/380996014_1124269575626822_4277449062801180984_n.jpg?stp=dst-jpg_p843x403&_nc_cat=106&ccb=1-7&_nc_sid=5614bc&_nc_ohc=u3rkn12Gp_wAX8t0Tda&_nc_ht=scontent.fhan5-3.fna&oh=00_AfAG7fnghDIf8PuIILVPkc9NpwsytoR64xVkwNwPpwe-eQ&oe=6516FF24';
 
 
 
