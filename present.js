9fa0e5cea33c501f4a25b72372f5af0308f097af
Nguyenvantuan <133848204+Tuanbautroi@users.noreply.github.com>
2023-09-26 17:18:46 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,7 +1,7 @@
 
 var to = '🐷!';
 var gift_url = 'https://www.facebook.com/nguyenbaolan1.10.20';
-var gift_image_url = 'https://scontent.fhan5-7.fna.fbcdn.net/v/t1.6435-9/120195184_2811273065864515_3816484128762948893_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_ohc=8PVP5WXe6tQAX_Piz3j&_nc_ht=scontent.fhan5-7.fna&oh=00_AfBFNAypTyQ2RmzTMf3F7sACoX2ZExfQ7_k7t2FWyrVBUw&oe=653A2B20';
+var gift_image_url = '';
 
 
 
